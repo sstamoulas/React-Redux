8bdeeb418c8dc80ea8fa26142acd81d2b3f5ffaa
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2018-10-07 19:32:21 +0300

    Setting up Redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, combineReducers } from "redux";
 
-import "./styles.css";
+//action creator
+const addItem = item => {
+  return {
+    type: "ADD_ITEM",
+    item: item
+  };
+};
+
+//reducer
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      return [...state, action.item];
+    default:
+      return state;
+  }
+};
+
+var store = createStore(reducer);
+
+console.log(store.dispatch(addItem("a")));
+console.log(store.getState());
+console.log(store.dispatch(addItem("b")));
+console.log(store.getState());
+console.log(store.dispatch(addItem("c")));
+console.log(store.getState());
 
 function App() {
   return (
